7925c841f358dd77655f0dfc8797209f7ce9b0de
lsheehan89 <95146672+lsheehan89@users.noreply.github.com>
2021-12-05 16:40:44 +0000

    Showing current temp, humidity, wind speed

---
+++ src/index.js
@@ -0,0 +1,20 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "d7c41983f20408f75c3e01482243112d";
+
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
